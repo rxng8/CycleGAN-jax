f4a2dc71f95ef903d4d3812e84a8ef992896b444
Viet Dung Nguyen <alexvn.edu@gmail.com>
2024-05-31 14:22:20 -0400

    update cycle gan and installation scripts

---
+++ README.md
@@ -0,0 +1,8 @@
+# CycleGAN
+
+
+```bash
+conda create -n gan python=3.10
+conda activate gan
+bash install.sh
+```
---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -l
+
+
+# Install required libraries
+# This already include jax, tf, tfprob
+pip install git+https://github.com/rxng8/embodied.git@main
